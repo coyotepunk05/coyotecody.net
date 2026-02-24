655e293ee89a4946b17338271bdc82c7d2984922
coyotepunk05 <coyote582005@gmail.com>
2026-02-23 20:25:57 -0800

    php

--- jellykill.php
+++ jellykill.php
@@ -1,18 +1,25 @@
 <?php
-$status_title = "Jellyfin Status Check";
-
-// Check the image server-side
 $imgUrl = "https://media.coyotecody.net/web/banner-light.b113d4d1c6c07fcb73f0.png";
 
-$headers = @get_headers($imgUrl);
-$isUp = $headers && strpos($headers[0], '200') !== false;
+$ch = curl_init($imgUrl);
+curl_setopt_array($ch, [
+    CURLOPT_NOBODY        => true,   // HEAD request, don't download the image
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_TIMEOUT       => 5,
+    CURLOPT_FOLLOWLOCATION => false, // Don't follow redirects - a redirect means it's NOT truly up
+    CURLOPT_SSL_VERIFYPEER => false,
+]);
+curl_exec($ch);
+$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
 
-// Now set embed values based on actual result
+$isUp = ($code === 200);
+
+$status_title   = "Jellyfin Status Check";
 $embed_description = $isUp ? "✅ Jellyfin is up!" : "❌ Jellyfin is down!";
-$embed_gif = $isUp
-    ? "https://coyotecody.net/images/flourish.gif"
-    : "https://coyotecody.net/images/jellykill.gif";
-$embed_color = $isUp ? "#16a34a" : "#dc2626";
+$embed_gif      = $isUp ? "https://coyotecody.net/images/flourish.gif" : "https://coyotecody.net/images/jellykill.gif";
+$embed_color    = $isUp ? "#16a34a" : "#dc2626";
+$status_text    = $isUp ? "jelly flourish" : "jellykill";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,7 +37,7 @@ $embed_color = $isUp ? "#16a34a" : "#dc2626";
         <div id="status_container">
             <img src="<?php echo $embed_gif; ?>" style="max-width: 300px; margin-bottom: 10px;">
             <p style="color: <?php echo $embed_color; ?>; font-weight: bold; font-family: sans-serif;">
-                <?php echo $embed_description; ?>
+                <?php echo $status_text; ?>
             </p>
         </div>
         <button class="button" onclick="location.href='./index.html'">mmmmrowww (back)</button>
