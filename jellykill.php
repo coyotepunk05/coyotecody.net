4f343c1a12478f609ebd9d0089b5facf5269a11a
coyotepunk05 <coyote582005@gmail.com>
2026-02-23 19:53:20 -0800

    php4

--- jellykill.php
+++ jellykill.php
@@ -1,16 +1,23 @@
 <?php
 // 1. The "Check": See if the Jellyfin banner is reachable
-$check_url = 'https://jellyfin.olgi.net/web/banner-light.b113d4d1c6c07fcb73f0.png';
-$headers = @get_headers($check_url);
+$check_url = 'https://media.coyotecody.net/web/banner-light.b113d4d1c6c07fcb73f0.png';
+$context = stream_context_create(['http' => ['timeout' => 3]]);
+$headers = @get_headers($check_url, 0, $context);
 $is_up = ($headers && strpos($headers[0], '200') !== false);
 
 // 2. Set variables based on status
 $status_title = $is_up ? "Jellyfin is UP" : "Jellyfin is DOWN";
 $status_desc  = $is_up ? "jelly flourish" : "jellykill";
-$status_gif   = $is_up 
-    ? "https://media.discordapp.net/attachments/883081538596642826/1375096957374038186/attachment.gif" 
-    : "https://media.discordapp.net/attachments/883081538596642826/1374210725496488017/attachment.gif";
-$theme_color  = $is_up ? "#52AD5B" : "#FF4C4C";
+
+// Local paths to your images in the \images folder
+$status_gif   = $is_up ? "./images/flourish.gif" : "./images/jellykill.gif";
+
+// Absolute URL for the Discord Embed
+$embed_gif    = $is_up 
+    ? "https://coyotecody.net/images/flourish.gif" 
+    : "https://coyotecody.net/images/jellykill.gif";
+
+$theme_color  = $is_up ? "#aa5ccc" : "#ff4c4c";
 ?>
 
 <!DOCTYPE html>
@@ -22,7 +29,7 @@ $theme_color  = $is_up ? "#52AD5B" : "#FF4C4C";
 
     <meta property="og:title" content="<?php echo $status_title; ?>">
     <meta property="og:description" content="<?php echo $status_desc; ?>">
-    <meta property="og:image" content="<?php echo $status_gif; ?>">
+    <meta property="og:image" content="<?php echo $embed_gif; ?>">
     <meta name="theme-color" content="<?php echo $theme_color; ?>">
     <meta property="og:type" content="website">
 
@@ -31,15 +38,10 @@ $theme_color  = $is_up ? "#52AD5B" : "#FF4C4C";
 <body>
     <div style="padding: 20px;">
         <div id="status">
-            <?php if ($is_up): ?>
-                <img src="https://media.discordapp.net/attachments/883081538596642826/1375096957374038186/attachment.gif" 
-                     style="display: block; max-width: 300px; margin-bottom: 10px;">
-                <p style="color: white; font-weight: bold;">jelly flourish</p>
-            <?php else: ?>
-                <img src="https://media.discordapp.net/attachments/883081538596642826/1374210725496488017/attachment.gif" 
-                     style="display: block; max-width: 300px; margin-bottom: 10px;">
-                <p style="color: #FF4C4C; font-weight: bold;">jellykill</p>
-            <?php endif; ?>
+            <img src="<?php echo $status_gif; ?>" alt="" style="display: block; max-width: 300px; margin-bottom: 10px;">
+            <p style="color: white; font-weight: bold; font-family: sans-serif;">
+                <?php echo $status_desc; ?>
+            </p>
         </div>
 
         <button class="button" onclick="location.href='./index.html'">mmmmrowww (back)</button>
